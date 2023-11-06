f3cd3a4d5e0fb0e16eaf2ca05cb65f31fd23c4e5
Prince Thind <princethind799@gmail.com>
2023-11-06 22:35:57 +0530

    update code

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-const { keyboard, Key } = require('@nut-tree/nut-js');
+const { keyboard, Key } = require("@nut-tree/nut-js");
 
 async function simulateCtrlAltE() {
   // Press and hold Ctrl, Alt, then press E, and finally release Alt and Ctrl.
-  const k=keyboard;
+  const k = keyboard;
   await k.pressKey(Key.LeftControl, Key.LeftAlt);
-  await k.type( 'e');
-  await k.releaseKey(Key.LeftControl, Key.LeftAlt,);
-
+  await k.type(process.argv[2] ?? "e");
+  await k.releaseKey(Key.LeftControl, Key.LeftAlt);
 }
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-
-simulateCtrlAltE()
-  .then(() => console.log('Ctrl+Alt+E simulated successfully'))
-  .catch(err => console.error('Error simulating Ctrl+Alt+E:', err));
-
+simulateCtrlAltE().catch((err) =>
+  console.error("Error simulating Ctrl+Alt+E:", err)
+);
